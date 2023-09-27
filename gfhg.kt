88e8ddf80f65ebe27dd1808bf76a39c15b3f9475
DIANNA CADEN <44316764+BOJ-ios@users.noreply.github.com>
2023-09-27 11:48:46 +0900

    2023-09-27

--- gfhg.kt
+++ gfhg.kt
@@ -1,18 +1,52 @@
+class User(val name: String) {
+    init {
+        println("주 생성자: 이름은 $name 입니다.")
+    }
+    constructor(name: String, age: Int) : this(name) {
+        println("보조 생성자: 이름은 $name, 나이는 $age 입니다.")
+    }
+    constructor(name: String, age: Int, height: Int) : this(name, age){
+        println("보조 생성자: 이름은 $name, 나이는 $age, 키는 $height 입니다.")
+    }
+    constructor() : this("Unknown") {
+        println("보조 생성자: 이름 정보가 없습니다.")
+    }
+}
+data class DataClass(val name: String, val email: String, val age: Int) {
+    lateinit var address: String
+    constructor(name: String, email: String, age: Int, address: String) : this(name, email, age) {
+        this.address = address
+    }
+}
+
 fun main() {
-  var list = listOf<Int>(10,20,30)
-  for (i in list) {
-    println(i)
-  }
-} 
+    val obj1 = DataClass("kkang", "A@a.com", 10, "Seoul")
+    val obj2 = DataClass("kkang", "A@a.com", 10, "Busan")
+    println("obj1.equals(obj2): ${obj1 == obj2}") 
 
-open class Super(name: String) {
-}
-class Sub(name: String): Super(name) {
-}
 
-open class Super2(name: String) {
-}
-class Sub2: Super2 {
-	constructor(name: String): super(name) {
-	}
+
+    // 1. 주 생성자를 사용하여 객체 생성
+    val user1 = User("John")
+    println();
+    // 2. 첫 번째 보조 생성자를 사용하여 객체 생성
+    val user2 = User("Jane", 25)
+    println();
+    // 3. 두 번째 보조 생성자를 사용하여 객체 생성
+    val user3 = User("Emily", 30, 160)
+    println();
+    // 4. 세 번째 보조 생성자를 사용하여 객체 생성
+    val user4 = User()
+
+    // 1. 람다 함수 사용 예제
+    val square: (Int) -> Int = { x -> x * x }
+    println("제곱: ${square(5)}")  // 출력: "제곱: 25"
+
+    // 2. 고차 함수 사용 예제
+    fun operateOnNumber(a: Int, b: Int, operation: (Int, Int) -> Int): Int {
+        return operation(a, b)
+    }
+    
+    val sum = operateOnNumber(5, 3) { x, y -> x + y }
+    println("합: $sum")  // 출력: "합: 8"
 }
